2b4695717f0f7e09259b97fd91e74b328ef6beb3
Alex Meleshko <meleha.alex@gmail.com>
2023-05-04 13:42:57 +0300

    Fix Screen sealed class

--- app/src/main/java/com/example/aventi/Screen.kt
+++ app/src/main/java/com/example/aventi/Screen.kt
@@ -1,16 +1,14 @@
 package com.example.aventi
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
 sealed class Screen(val route: String?, val title: String?, val icon: ImageVector)  {
-    class Home(): Screen("home", "Home", Icons.Default.Home)
-    class Profile(): Screen("profile", "Profile", Icons.Default.Person)
-    class Compass(): Screen("compass", "Compass", Icons.Default.PlayArrow)
-    class Messages(): Screen("messages", "Messages", Icons.Default.Email)
+    object Home: Screen("home", "Home", Icons.Default.Home)
+    object Profile: Screen("profile", "Profile", Icons.Default.Person)
+    object Compass: Screen("compass", "Compass", Icons.Default.PlayArrow)
+    object Messages: Screen("messages", "Messages", Icons.Default.Email)
+    object Add: Screen("add", "Add", Icons.Default.Add)
 }
